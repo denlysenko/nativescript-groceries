571c95ae532bd72031d691a0050399a466916222
den.lysenko <den.l@april33.com>
2018-01-28 20:24:57 +0200

    full-app

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { User } from './shared/user/user';
+import { UserService } from './shared/user/user.service';
+
+@Component({
+  selector: 'main',
+  template: '<page-router-outlet></page-router-outlet>'
+})
+export class AppComponent {}
---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Page } from 'ui/page';
+import { Color } from 'color';
+import { View } from 'ui/core/view';
+import { TextField } from 'ui/text-field';
+
+import { User } from '../../shared/user/user';
+import { UserService } from '../../shared/user/user.service';
+import { setHintColor } from '../../utils/hint-util';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './pages/login/login.html',
+  styleUrls: ['pages/login/login-common.css', 'pages/login/login.css'],
+  providers: [UserService]
+})
+export class LoginComponent implements OnInit {
+  user: User;
+  isLoggingIn = true;
+
+  @ViewChild('container') container: ElementRef;
+  @ViewChild('email') email: ElementRef;
+  @ViewChild('password') password: ElementRef;
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private page: Page
+  ) {
+    this.user = new User();
+    this.user.email = 'denny.lysenko@gmail.com';
+    this.user.password = 'password';
+  }
+
+  ngOnInit() {
+    this.page.actionBarHidden = true;
+    this.page.backgroundImage = 'res://bg_login';
+  }
+
+  submit() {
+    if (!this.user.isValidEmail()) {
+      return alert('Enter valid email address.');
+    }
+
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+    this.setTextFieldColors();
+    const container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn
+        ? new Color('white')
+        : new Color('#301217'),
+      duration: 200
+    });
+  }
+
+  login() {
+    this.userService
+      .login(this.user)
+      .subscribe(
+        () => this.router.navigate(['/list']),
+        error => alert('Unfortunately we could not find your account.')
+      );
+  }
+
+  signUp() {
+    this.userService.register(this.user).subscribe(
+      () => {
+        alert('Your account was successfully created.');
+        this.toggleDisplay();
+      },
+      () => alert('Unfortunately we were unable to create your account.')
+    );
+  }
+
+  setTextFieldColors() {
+    let emailTextField = <TextField>this.email.nativeElement;
+    let passwordTextField = <TextField>this.password.nativeElement;
+
+    let mainTextColor = new Color(this.isLoggingIn ? 'black' : '#C4AFB4');
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+
+    let hintColor = new Color(this.isLoggingIn ? '#ACA6A7' : '#C4AFB4');
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
+  }
+}
